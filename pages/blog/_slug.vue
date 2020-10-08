32030272fd6c67ad9672a34fbf3e4a4a9b2401ed
Dennis O'Connell <doconnell@scu.edu>
2020-10-08 12:49:07 -0400

    created formatDate method and included date updated in my-first-blog-post

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,6 +4,12 @@
       // fetch our article here
         const article = await $content('articles', params.slug).fetch()
         return { article }
+    },
+    methods: {
+        formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+     }  
 
     }
   }
@@ -11,6 +17,8 @@
 
 <template>
   <article>
+      <pre> {{ article }} </pre>
+      <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
     <nuxt-content :document="article" />
   </article>
 </template>

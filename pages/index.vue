cf144655c76c78889782f35e73d53e6a2638073d
Dennis O'Connell <doconnell@scu.edu>
2020-10-13 19:19:30 -0400

    imported tailwind, generated static files

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,69 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        socialdistancedsj
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="max-wscreel-xl mx-auto">
+
+    <!-- header -->
+   <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Examples
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Blog
+      </a>
     </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div>
+  </div>
+</nav>
+    <!-- header ends here -->
+
+
+    
+
+
+
+     <!-- footer -->
+    <footer class="border-t mt-32 pt-12 pb-32 px-4 lg:px-0">
+      <div class="flex">
+
+        <div class="w-full md:w-1/3 lg:w-1/4">
+          <h6 class="font-semibold text-gray-700 mb-4">Company</h6>
+          <ul>
+            <li> <a href="" class="block text-gray-600 py-2">Team</a> </li>
+            <li> <a href="" class="block text-gray-600 py-2">About us</a> </li>
+            <li> <a href="" class="block text-gray-600 py-2">Press</a> </li>
+          </ul>
+        </div>
+
+        <div class="w-full md:w-1/3 lg:w-1/4">
+          <h6 class="font-semibold text-gray-700 mb-4">Content</h6>
+          <ul>
+            <li> <a href="" class="block text-gray-600 py-2">Blog</a> </li>
+            <li> <a href="" class="block text-gray-600 py-2">Privacy Policy</a> </li>
+            <li> <a href="" class="block text-gray-600 py-2">Terms & Conditions</a> </li>
+            <li> <a href="" class="block text-gray-600 py-2">Documentation</a> </li>
+          </ul>
+        </div>
+
+      </div>
+    </footer>
+    <!-- footer ends here -->
+
+
   </div>
 </template>
 
